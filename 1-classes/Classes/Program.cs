8dc5f52acc27c6a1a1b80d1e1000cf3ac3bd9278
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-18 16:07:24 +0300

    Intoduce classes

---
+++ 1-classes/Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            var p = person.Parse("John");
+            person.Name = "John";
+            person.Introduce("George");
+        }
+    }
+}
