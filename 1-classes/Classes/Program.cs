8a9c39897c486720551c7a74a5f418c43aac43d3
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-19 08:32:08 +0300

    Use methods

--- 1-classes/Classes/Program.cs
+++ 1-classes/Classes/Program.cs
@@ -6,22 +6,9 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-            // var person = Person.Parse("John");
-            // person.Introduce("George");
-
-            var customer = new Customer(1, "John");
-
-            // var order = new Order();
-            // customer.Orders.Add(order);
-
-            // Console.WriteLine(customer.Id);
-            // Console.WriteLine(customer.Name);
-
-            var person = new Person
-            {
-                Name = "John"
-            };
-            person.Introduce("George");
+            var point = new Point(10, 20);
+            point.Move(new Point(40, 60));
+            Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
         }
     }
 }
