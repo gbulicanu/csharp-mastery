303aa4ef5fae2456235211e72c9b64c466171703
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-19 22:12:56 +0300

    Use inhertance

---
+++ 2-associations-between-classes/Inheritance/PresentationObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class PresentationObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public void Copy() 
+        {
+            Console.WriteLine("Copy");
+        }
+
+        public void Duplicate() 
+        {
+            Console.WriteLine("Duplicate");
+        }
+    }
+}
---
+++ 2-associations-between-classes/Inheritance/Text.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Text : PresentationObject
+    {
+        public int FontSize { get; set; }
+        public string FontName { get; set; }
+
+        public void AddHyperlink(string url) 
+        {
+            Console.WriteLine(url);
+        }
+        
+    }
+}
