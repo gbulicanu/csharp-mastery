f3da1c12f47375611cc175827db474f8acdf44fb
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-20 17:32:00 +0300

    Fix

--- 3-inheritance/AccessModifiers/Program.cs
+++ 3-inheritance/AccessModifiers/Program.cs
@@ -1,14 +1,7 @@
 ﻿using System;
 
 namespace AccessModifiers
-{
-    public class GoldCustomer : Customer
-    {
-        public void OfferVoucher() 
-        {
-            var rating = this.CalculateRating(true);
-        }
-    }
+{ 
     class Program
     {
         static void Main(string[] args)
