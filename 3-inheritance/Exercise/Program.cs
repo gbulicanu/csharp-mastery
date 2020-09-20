5e1936acc9dcc4ea50679da41c8e8a11d9d4fab5
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-20 23:40:29 +0300

    Add solution to exercise - design a stack

---
+++ 3-inheritance/Exercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercise
+{
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            //stack.Pop();
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            try
+            {
+                //stack.Clear();
+                stack.Pop();
+            }
+            catch (Exception) 
+            {
+                Console.WriteLine("Throws as expected ...");
+            }
+            try
+            {
+                stack.Push(null);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Throws as expected ...");
+            }
+        }
+    }
+}
---
+++ 3-inheritance/Exercise/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace Exercise
+{
+    partial class Program
+    {
+        public class Stack
+        {
+            private ArrayList list = new ArrayList();
+
+            public void Push(object obj)
+            {
+                if(obj == null)
+                {
+                    throw new InvalidOperationException("Cannot push null reference.");
+                }
+
+                list.Add(obj);
+            }
+
+            public object Pop() 
+            {
+                if (list.Count == 0) 
+                {
+                    throw new InvalidOperationException("Cannot pop the empty stack.");
+                }
+
+                var lastIndex = list.Count - 1;
+                var lastAddedItem = list[lastIndex];
+                list.RemoveAt(lastIndex);
+
+                return lastAddedItem;
+            }
+
+            public void Clear() 
+            {
+                list.Clear();
+            }
+        }
+    }
+}
