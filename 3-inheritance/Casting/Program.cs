5ead846e1d573afb46754793634793e54c9fbc39
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-20 18:38:44 +0300

    Use upcasting

---
+++ 3-inheritance/Casting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Casting
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            // Upcastign
+            Shape shape = text;
+
+            text.Width = 200;
+            shape.Width = 100;
+
+            Console.WriteLine(text.Width);
+        }
+    }
+}
