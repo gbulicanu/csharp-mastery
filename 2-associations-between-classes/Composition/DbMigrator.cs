33c8b135a91456626f45c03d909ce49956ea1b0b
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-20 17:07:25 +0300

    Use composition over inheritance

---
+++ 2-associations-between-classes/Composition/DbMigrator.cs
@@ -0,0 +1,17 @@
+﻿namespace Composition
+{
+    public class DbMigrator
+    {
+        private readonly Logger logger;
+
+        public DbMigrator(Logger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Migrate() 
+        {
+            this.logger.Log("Migrating database to new version ...");
+        }
+    }
+}
--- 2-associations-between-classes/Composition/Program.cs
+++ 2-associations-between-classes/Composition/Program.cs
@@ -1,11 +1,16 @@
-﻿using System;
-
-namespace Composition
+﻿namespace Composition
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var dbMigrator = new DbMigrator(new Logger());
+
+            var logger = new Logger();
+            var installer = new Installer(logger);
+
+            dbMigrator.Migrate();
+            installer.Install();
         }
     }
 }
