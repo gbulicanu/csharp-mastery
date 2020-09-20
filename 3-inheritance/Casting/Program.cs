fdb889d37217c9611990615d5eff559fcc5c335b
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-20 18:42:21 +0300

    Add more upcasting

--- 3-inheritance/Casting/Program.cs
+++ 3-inheritance/Casting/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Casting
 {
@@ -6,14 +9,24 @@ namespace Casting
     {
         static void Main(string[] args)
         {
-            var text = new Text();
-            // Upcastign
-            Shape shape = text;
+            //var text = new Text();
+            //// Upcastign
+            //Shape shape = text;
 
-            text.Width = 200;
-            shape.Width = 100;
+            //text.Width = 200;
+            //shape.Width = 100;
 
-            Console.WriteLine(text.Width);
+            //Console.WriteLine(text.Width);
+
+            // StreamReader streamReader = new StreamReader(new MemoryStream());
+
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("George");
+            list.Add(new Text());
+
+            var anotherList = new List<Shape>();
+            anotherList.Add(new Text());
         }
     }
 }
