90c44fcd121aaeeed571b770f9249cb02f20d93e
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-20 22:50:12 +0300

    Use boxing/unboxing

---
+++ 3-inheritance/Boxing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            
+            // Boxing here
+            list.Add(1);
+            
+            list.Add("George");
+            
+            // Boxing here
+            list.Add(DateTime.Today);
+
+            // Unboxing
+            var number = (int)list[0];
+            Console.WriteLine(number.ToString());
+        }
+    }
+}
