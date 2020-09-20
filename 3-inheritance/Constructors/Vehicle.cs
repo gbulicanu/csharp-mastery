95dd5b2242c32e435d7f1924e6d293ed7c4cd24c
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-20 18:15:17 +0300

    Use inheritance - constructors

---
+++ 3-inheritance/Constructors/Vehicle.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Constructors
+{
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+            Console.WriteLine("Vehicle is being initialized.");
+        }
+    }
+}
