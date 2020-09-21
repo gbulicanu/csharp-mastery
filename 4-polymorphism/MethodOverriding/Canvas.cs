d4196dcf687a80bebfb98177058411f6186658b5
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-21 13:19:02 +0300

    Add poorly writen code

---
+++ 4-polymorphism/MethodOverriding/Canvas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodOverriding
+{
+    public class Canvas
+    {
+        public void DrawShapes(List<Shape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                switch (shape.Type) 
+                {
+                    case ShapeType.Circle:
+                        Console.WriteLine("Draw a circle");
+                        break;
+                    case ShapeType.Reactangle:
+                        Console.WriteLine("Draw a ractangle");
+                        break;
+                }
+            }
+        }
+    }
+}
