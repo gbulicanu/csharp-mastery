c7fc31a6a550398317fd56859f3a5a8a4ee800ea
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-21 21:42:21 +0300

    Use extensibility - introduce new implementation for logger

---
+++ 5-interfaces/Extensibility/FileLogger.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace Extensibility
+{
+    public class FileLogger : ILogger
+    {
+        private readonly string path;
+
+        public FileLogger(string path)
+        {
+            this.path = path;
+        }
+
+        public void LogError(string message)
+        {
+            using (var streamWriter = new StreamWriter(path, true))
+            {
+                streamWriter.WriteLine("ERROR: {0}", message);
+            }
+        }
+
+        public void LogInfo(string message)
+        {
+            using (var streamWriter = new StreamWriter(path, true))
+            {
+                streamWriter.WriteLine(message);
+            }
+        }
+    }
+}
