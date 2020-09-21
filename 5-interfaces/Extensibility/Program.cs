b620d8f800f8e51fdb3e8d702b73ebe7a56a0a00
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-21 21:53:06 +0300

    Extensibility - swap implementation without changing code in DbMigrator

--- 5-interfaces/Extensibility/Program.cs
+++ 5-interfaces/Extensibility/Program.cs
@@ -6,7 +6,8 @@ namespace Extensibility
     {
         static void Main(string[] args)
         {
-            var dbMigrator = new DbMigrator(new ConsoleLogger());
+            //var dbMigrator = new DbMigrator(new ConsoleLogger());
+            var dbMigrator = new DbMigrator(new FileLogger("extensibility.log"));
 
             dbMigrator.Migrate();
         }
