ac48fe973dae0045f8bbc5af1463c155f75a3e1a
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-21 13:54:41 +0300

    Add support for triangle shapes

--- 4-polymorphism/MethodOverriding/Program.cs
+++ 4-polymorphism/MethodOverriding/Program.cs
@@ -9,6 +9,7 @@ namespace MethodOverriding
             var shapes = new List<Shape>();
             shapes.Add(new Circle { Width = 100, Height = 100 });
             shapes.Add(new Rectangle { Width = 100, Height = 30 });
+            shapes.Add(new Triangle());
 
             var canvas = new Canvas();
             canvas.DrawShapes(shapes);
