c0d48cd011278a22380c183d2b1d3c4f72cf31d5
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-09-21 14:11:54 +0300

    Use abstract modifier - to enforce some implemention in derived classes

--- 4-polymorphism/AbstractClasses/Rectangle.cs
+++ 4-polymorphism/AbstractClasses/Rectangle.cs
@@ -1,6 +1,12 @@
-﻿namespace AbstractClasses
+﻿using System;
+
+namespace AbstractClasses
 {
     public class Rectangle : Shape
     {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a rectangle");
+        }
     }
 }
